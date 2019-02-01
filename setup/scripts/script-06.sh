b81d58329428feb5a518252992cf7f35d32bd5a1
Ulf Troppens <troppens@gmx.net>
2019-02-01 18:55:16 +0100

    Create Spectrum Scale filesystem
    Create Spectrum Scale filesystem.

---
+++ setup/scripts/script-06.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+# Exit on error
+set -e
+
+# Improve readability of output
+echo "========================================================================================="
+echo "===>"
+echo "===> Running $0"
+echo "===> Create Spectrum Scale filesystems"
+echo "===>"
+echo "========================================================================================="
+
+# Print commands and their arguments as they are executed
+set -x
+
+# Exit script immediately, if one of the commands returns error code
+set -e
+
+
+# Show Spectrum Scale filesystem specification
+echo "===> Show filesystem specification"
+sudo /usr/lpp/mmfs/5.0.2.2/installer/spectrumscale filesystem list
+
+# Create Spectrum Scale filesystems
+echo "===> Create Spectrum Scale filesystems"
+sudo /usr/lpp/mmfs/5.0.2.2/installer/spectrumscale deploy
+
+# Show Spectrum Scale filesystem configuration
+echo "===> Show Spectrum Scale filesystem configuration"
+sudo mmlsfs all
+
+# Show Spectrum Scale filesystem usage
+echo "===> Show Spectrum Scale filesystem usage"
+sudo mmdf fs1
+
+
+# Exit successfully
+echo "===> Script completed successfully!"
+exit 0
