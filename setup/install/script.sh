a79cd9bbc4fac56a37777bba54258853afe2860e
Ulf Troppens <troppens@gmx.net>
2019-02-03 19:41:43 +0100

    Renamed directory
    Renamed scripts/ to install/ to distinguish from the to be created demo/ directory.

--- setup/install/script.sh
+++ setup/install/script.sh
@@ -16,12 +16,12 @@ set -e
 
 
 # Perform all steps to provision Spectrum Scale Cluster
-/vagrant/scripts/script-01.sh
-/vagrant/scripts/script-02.sh
-/vagrant/scripts/script-03.sh
-/vagrant/scripts/script-04.sh
-/vagrant/scripts/script-05.sh
-/vagrant/scripts/script-06.sh
+/vagrant/install/script-01.sh
+/vagrant/install/script-02.sh
+/vagrant/install/script-03.sh
+/vagrant/install/script-04.sh
+/vagrant/install/script-05.sh
+/vagrant/install/script-06.sh
 
 
 # Exit successfully
