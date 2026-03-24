b96268dbd90d38b7950550fc5f597735eaf1ac7e
hseipp <20093686+hseipp@users.noreply.github.com>
2026-03-24 11:28:01 +0100

    Storage Scale 6.0.0.2, REST API v3, new prep-box
    Added Storage Scale 6.0.0.2.
    Enabled Scaleadmd so that new REST API v3 can be used.
    Adopted documentation to new REST API.
    Switched to updated Rocky Linux base image.
    
    Signed-off-by: hseipp <20093686+hseipp@users.noreply.github.com>

--- setup/install/script-05.sh
+++ setup/install/script-05.sh
@@ -4,6 +4,21 @@ TASK="Install Storage Scale and create a Storage Scale cluster"
 
 source /vagrant/install/common-preamble.sh
 
+# Create certificate required for REST API v3
+echo "===> Create certificate required for REST API v3"
+sudo /usr/lpp/mmfs/$VERSION/ansible-toolkit/spectrumscale scaleadmd enable
+sudo mkdir /root/tls
+sudo openssl ecparam -name prime256v1 -genkey -noout -out /root/tls/ca.key
+sudo openssl req -new -x509 -sha256 -key /root/tls/ca.key -out /root/tls/ca.crt -subj "/C=DE/ST=NewYork/L=Armonk/O=IBM/CN=`hostname`" -days 10000
+sudo openssl ecparam -name prime256v1 -genkey -noout -out /root/tls/server.key
+sudo openssl req -new -sha256 -key /root/tls/server.key -out /root/tls/server.csr -subj "/C=DE/ST=NewYork/L=Armonk/O=IBM/CN=demo.example.com"
+sudo openssl x509 -req -in /root/tls/server.csr -CA /root/tls/ca.crt -CAkey /root/tls/ca.key -CAcreateserial -out /root/tls/server.pem -days 10000 -sha256
+sudo openssl verify -CAfile /root/tls/ca.crt /root/tls/server.pem
+sudo /usr/lpp/mmfs/$VERSION/ansible-toolkit/spectrumscale nodeid define --cert /root/tls/server.pem --key /root/tls/server.key --chain /root/tls/ca.crt -N all
+sudo firewall-cmd --add-port=50052/tcp --permanent
+sudo firewall-cmd --add-port=46443/tcp --permanent
+sudo firewall-cmd --reload
+
 # Install Storage Scale and create Storage Scale cluster
 echo "===> Install Storage Scale and create Storage Scale cluster"
 sudo /usr/lpp/mmfs/$VERSION/ansible-toolkit/spectrumscale install
@@ -54,7 +69,20 @@ sudo /usr/lpp/mmfs/gui/cli/initgui
 
 # Issue #58: Immediately create a GUI user
 echo "==> Create GUI user for monitoring purposes"
-sudo /usr/lpp/mmfs/gui/cli/mkuser performance -g Monitor -p monitor
+sudo /usr/lpp/mmfs/gui/cli/mkuser performance -g Monitor -p monitoring
+
+# Check REST API v3
+sudo scalectl apihealth get m1.example.com
+sudo scalectl authorization cani -a create -r '/scalemgmt/v3/filesystems'
+sudo mmcommon checkscaleapienabled
+sudo useradd performance
+echo "monitoring" | sudo passwd --stdin performance
+sudo useradd admin
+echo "supersecret" | sudo passwd --stdin admin
+sudo scalectl authorization domain update StorageScaleDomain -F /vagrant/files/spectrumscale/restapi-policy.json
+# The following commands list the authorization domains in detail, skipping here as too verbose
+#sudo scalectl authorization domain list
+#sudo scalectl authorization domain get StorageScaleDomain
 
 # Exit successfully
 echo "===> Script completed successfully!"
